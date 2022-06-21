ec928f89b967ea4796470ddb51590869ec3bc407
Bradly landucci <bradly.landucci.5149@mail.linnbenton.edu>
2022-06-20 21:48:15 -0700

    First commit

---
+++ project.mk
@@ -0,0 +1,17 @@
+# #############################################################################
+# Project Customization
+# #############################################################################
+
+PROJECT = SimpleDelay
+
+UCSRC =
+
+UCXXSRC = ./src/simpledelay.cpp
+
+UINCDIR =
+
+UDEFS =
+
+ULIB = 
+
+ULIBDIR =
---
+++ src/simpledelay.cpp
@@ -0,0 +1,74 @@
+#include "userdelfx.h"
+#include "delayline.hpp"
+
+#define BUF_SIZE 48000
+
+static dsp::DelayLine s_delay;
+static __sdram float s_delay_ram[BUF_SIZE];
+
+uint32_t s_len;
+
+static float s_mix;
+static float gain;
+static int w_pos;
+
+float bpm;
+
+void DELFX_INIT(uint32_t platform, uint32_t api)
+{
+  s_delay.setMemory(s_delay_ram, BUF_SIZE);  
+  s_delay.clear();
+  bpm = _fx_get_bpmf();
+  s_len = (60 / bpm * 48000) * 32;
+  s_mix = .5f;
+}
+
+void DELFX_PROCESS(float *xn, uint32_t frames)
+{
+  const float dry = 1.f - s_mix;
+  const float wet = s_mix;
+
+  for (int i=0;i<frames*2;i++)
+  {
+    const float delSample = gain * s_delay.read(s_len);
+    xn[i] = (dry * xn[i] + wet * delSample);
+    s_delay.write(xn[i] + wet * delSample);
+  }
+}
+
+void DELFX_PARAM(uint8_t index, int32_t value)
+{
+  const float valf = q31_to_f32(value);
+  switch (index) {
+  case 0:
+    if (valf == 0.f) gain = 0.1f;
+    else gain = valf;
+    break;
+  case 1:
+
+    bpm = _fx_get_bpm();
+    if (valf < 0.25) 
+    {
+      s_len = (60 / bpm * 48000) * 32;
+    } 
+    else if (valf < 0.5) 
+    {
+      s_len = (60 / bpm * 48000) * 16;
+    } 
+    else if (valf < 0.75) 
+    {
+      s_len = (60 / bpm * 48000) * 8;
+    } 
+    else 
+    {
+      s_len = (60 / bpm * 48000) * 4;
+    }
+    break;
+
+  case 3:
+    s_mix = valf;
+    break;
+  default:
+    break;
+  }
+}
