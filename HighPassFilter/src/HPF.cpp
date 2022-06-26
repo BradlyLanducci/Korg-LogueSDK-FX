22e285c3d20d4b8d6a35dc61dbc77309f7c5443b
BradlyLanducci <salvesoundsofficial@gmail.com>
2022-06-26 15:34:55 -0700

    Implemented HPF with biquadratic filter.
    Now there is a cutoff knob.

--- HighPassFilter/src/HPF.cpp
+++ HighPassFilter/src/HPF.cpp
@@ -1,13 +1,24 @@
 #include "usermodfx.h"
 #include "fx_api.h"
+#include "biquad.hpp"
 
-float curSampleL = 0.f;
-float curSampleR = 0.f;
-float prevSampleL = 0.f;
-float prevSampleR = 0.f;
+static dsp::BiQuad filter_l, filter_r;
+float resonance = 1.4041f;
+float cutoff = 0.49f;
 
 void MODFX_INIT(uint32_t platform, uint32_t api)
 {
+  filter_l.flush();
+  filter_l.mCoeffs.setSOHP(fx_tanpif(cutoff), resonance);
+  filter_r.flush();
+  filter_r.mCoeffs = filter_l.mCoeffs;
+}
+
+float absolute(float valf)
+{
+  if (valf < 0)
+    return -valf;
+  return valf;
 }
 
 void MODFX_PROCESS(const float *xn, float *yn,
@@ -18,15 +29,37 @@ void MODFX_PROCESS(const float *xn, float *yn,
   {
     curSampleL = *xn++;
     curSampleR = *xn++;
-    *yn++ = curSampleL + (-1.f * prevSampleL);
-    *yn++ = curSampleR + (-1.f * prevSampleR);
-    prevSampleL = curSampleL;
-    prevSampleR = curSampleR;
+
+    curSampleL = filter_l.process_so(curSampleL);
+    curSampleR = filter_r.process_so(curSampleR);
+
+    *yn++ = curSampleL;
+    *yn++ = curSampleR;
   }
 }
 
 void MODFX_PARAM(uint8_t index, int32_t value)
 {
   const float valf = q31_to_f32(value);
-
+  float invertedValf;
+  float range = 0;
+  switch (index) 
+  {
+  case 0:
+    invertedValf = absolute(1-valf);
+    range = clipminmaxf(0.001f, invertedValf, 1.f);
+    cutoff = (1 / range) * 0.00046875;
+    filter_l.flush();
+    filter_l.mCoeffs.setSOHP(fx_tanpif(cutoff), resonance);
+    filter_r.flush();
+    filter_r.mCoeffs = filter_l.mCoeffs;
+    break;
+  case 1:
+    break;
+  case 3:
+    // Mix Val
+    break;
+  default:
+    break;
+  }
 }
