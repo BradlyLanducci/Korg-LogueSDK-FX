770ab5aa521905a2314eddfee1cca9fc8bc98ed7
BradlyLanducci <salvesoundsofficial@gmail.com>
2022-09-29 00:52:53 -0700

    Updated waveshaping algorithm and it works properly now.

--- Waveshaper/src/waveshaper.cpp
+++ Waveshaper/src/waveshaper.cpp
@@ -4,20 +4,31 @@
 #include "fx_api.h"
 
 // Initializing Params //
-static float distamt = 0.5f;
-static float wet = 0.5f;
+float distAmt;
 
 // Simple waveshaping algorithm //
-// x' = 3/2x - 1/2(x^3)
-// https://www.musicdsp.org/en/latest/Effects/114-waveshaper-simple-description.html
+// Audio Effects: Theory, Implementation and Application Pg. 184
 float __fast_inline waveshape(float in) 
 {
-    return 1.5f * in - 0.5f * in *in * in;
+  float threshold1 = 1.0f/3.0f;
+  float threshold2 = 2.0f/3.0f;
+
+  if(in > threshold2)
+    return 1.0f;
+  else if(in > threshold1)
+    return (3.0f - (2.0f - 3.0f*in) * (2.0f - 3.0f*in))/3.0f;
+  else if(in < -threshold2)
+    return -1.0f;
+  else if(in < -threshold1)
+    return -(3.0f - (2.0f + 3.0f*in) * (2.0f + 3.0f*in))/3.0f;
+  else
+    return 2.0f * in;
 }
 
 // Initializing Platform //
 void MODFX_INIT(uint32_t platform, uint32_t api)
 {
+  float distAmt = 0.5f;
 }
 
 // Main DSP Process Block //
@@ -26,20 +37,15 @@ void MODFX_PROCESS(const float *xn, float *yn,
                    const float *sub_xn, float *sub_yn,
                    uint32_t frames)
 {
-  float base_main;
-  float wet_signal;
-
   // Effect processing loop //
   // For double frames (AKA samples cause each frame = sample pair)
   for (int i=0;i<frames*2;i++)
   {
     float xn_cur = *xn++;
+    const float dry = 1.f - distAmt;
+    const float drive = 100.f;
 
-    // Waveshaping algorithm // 
-    // DRY + WET
-    wet_signal = (wet * ((xn_cur * ((distamt * 10.0f) + 1.f)) - xn_cur));
-    base_main = xn_cur + wet_signal;
-    *yn++ = waveshape(base_main);
+    *yn++ = (dry * xn_cur) + (distAmt * waveshape(drive * xn_cur));
   }
 }
 
@@ -51,10 +57,9 @@ void MODFX_PARAM(uint8_t index, int32_t value)
   switch (index)
   {
     case 0:
-      distamt = valf;
+      distAmt = valf;
       break;
     case 1:
-      wet = valf;
       break;
     default:
       break;
